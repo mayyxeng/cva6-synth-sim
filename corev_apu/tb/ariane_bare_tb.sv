921b289d4db8a68edeb3363d09359dc5e3221a88
Mahyar Emami <mayyxeng@gmail.com>
2023-06-29 14:15:34 +0000

    simpler tb

--- corev_apu/tb/ariane_bare_tb.sv
+++ corev_apu/tb/ariane_bare_tb.sv
@@ -24,11 +24,11 @@ module ariane_bare_tb #(
     logic COMMIT_TRACE;
     initial begin
         if(!$value$plusargs("MAX_CYCLES=%d", MAX_CYCLES)) MAX_CYCLES = 1000;
-        // if ($test$plusargs("TRACE")) begin
-        //     $display("Tracing to vlt_dump.vcd...");
-        //     $dumpfile("vlt_dump.vcd");
-        //     $dumpvars();
-        // end
+        if ($test$plusargs("TRACE")) begin
+            $display("Tracing to vlt_dump.vcd...");
+            $dumpfile("vlt_dump.vcd");
+            $dumpvars();
+        end
         verbose = $test$plusargs("VERBOSE");
         COMMIT_TRACE = verbose | $test$plusargs("TRACE");
     end
@@ -52,121 +52,12 @@ module ariane_bare_tb #(
     //  BUS
     // --------------
 
-    // AXI slave bus interface, connects to the core
-    AXI_BUS #(
-        .AXI_ADDR_WIDTH ( AXI_ADDRESS_WIDTH   ),
-        .AXI_DATA_WIDTH ( AXI_DATA_WIDTH      ),
-        .AXI_ID_WIDTH   ( ariane_soc::IdWidth ),
-        .AXI_USER_WIDTH ( AXI_USER_WIDTH      )
-    ) slave[ariane_soc::NrSlaves-1:0](); // one is unconnected, since there is no DM
-
     AXI_BUS #(
         .AXI_ADDR_WIDTH ( AXI_ADDRESS_WIDTH        ),
         .AXI_DATA_WIDTH ( AXI_DATA_WIDTH           ),
         .AXI_ID_WIDTH   ( ariane_soc::IdWidthSlave ),
         .AXI_USER_WIDTH ( AXI_USER_WIDTH           )
-    ) master[ariane_soc::NB_PERIPHERALS-1:0]();
-    // need only 3 masters: DRAM, ROM and GPIO but we instantiate more for no reason
-
-
-    // ---------------
-    // BOOT ROM
-    // ---------------
-    logic                         rom_req;
-    logic [AXI_ADDRESS_WIDTH-1:0] rom_addr;
-    logic [AXI_DATA_WIDTH-1:0]    rom_rdata;
-
-    axi2mem #(
-        .AXI_ID_WIDTH   ( ariane_soc::IdWidthSlave ),
-        .AXI_ADDR_WIDTH ( AXI_ADDRESS_WIDTH        ),
-        .AXI_DATA_WIDTH ( AXI_DATA_WIDTH           ),
-        .AXI_USER_WIDTH ( AXI_USER_WIDTH           )
-    ) i_axi2rom (
-        .clk_i  ( clk_i                   ),
-        .rst_ni ( rst_n                   ),
-        .slave  ( master[ariane_soc::ROM] ),
-        .req_o  ( rom_req                 ),
-        .we_o   (                         ),
-        .addr_o ( rom_addr                ),
-        .be_o   (                         ),
-        .user_o (                         ),
-        .data_o (                         ),
-        .user_i ( '0                      ),
-        .data_i ( rom_rdata               )
-    );
-
-    bootrom i_bootrom (
-        .clk_i      ( clk_i     ),
-        .req_i      ( rom_req   ),
-        .addr_i     ( rom_addr  ),
-        .rdata_o    ( rom_rdata )
-    );
-
-    // ----------------
-    // GPIO: a proxy for toHost fromHost
-    // ----------------
-
-    logic                         gpio_req;
-    logic                         gpio_we;
-    logic [AXI_ADDRESS_WIDTH-1:0] gpio_addr;
-    logic [AXI_DATA_WIDTH/8-1:0]  gpio_be;
-    logic [AXI_DATA_WIDTH-1:0]    gpio_wdata;
-    logic [AXI_DATA_WIDTH-1:0]    gpio_rdata;
-    logic [AXI_USER_WIDTH-1:0]    gpio_wuser;
-    logic [AXI_USER_WIDTH-1:0]    gpio_ruser;
-
-    axi2mem #(
-        .AXI_ID_WIDTH   ( ariane_soc::IdWidthSlave ),
-        .AXI_ADDR_WIDTH ( AXI_ADDRESS_WIDTH        ),
-        .AXI_DATA_WIDTH ( AXI_DATA_WIDTH           ),
-        .AXI_USER_WIDTH ( AXI_USER_WIDTH           )
-    ) i_axi2gpio (
-        .clk_i  ( clk_i                   ),
-        .rst_ni ( rst_n                   ),
-        .slave  ( master[ariane_soc::GPIO]),
-        .req_o  ( gpio_req                ),
-        .we_o   ( gpio_we                 ),
-        .addr_o ( gpio_addr               ),
-        .be_o   ( gpio_be                 ),
-        .user_o ( gpio_wuser              ),
-        .data_o ( gpio_wdata              ),
-        .user_i ( gpio_ruser              ),
-        .data_i ( gpio_rdata              )
-    );
-
-    always_ff @(posedge clk_i) begin
-        if (gpio_req) begin
-            if (gpio_we && gpio_be == {(AXI_DATA_WIDTH / 8){1'b1}}) begin
-                if (gpio_wdata[0] == 0) begin
-                    $display("@%d Wrong toHost code %h", cycle_count, gpio_wdata);
-                    $stop;
-                end else if (gpio_wdata != 1) begin
-                    $display("@%d test failed! toHost = %h", cycle_count, gpio_wdata);
-                    $stop;
-                end else begin
-                    $display("@%d test passed!", cycle_count);
-                    $finish;
-                end
-            end
-        end
-    end
-    // ------------------------------
-    // Memory + Exclusive Access
-    // ------------------------------
-
-    // AXI_BUS #(
-    //     .AXI_ADDR_WIDTH ( AXI_ADDRESS_WIDTH        ),
-    //     .AXI_DATA_WIDTH ( AXI_DATA_WIDTH           ),
-    //     .AXI_ID_WIDTH   ( ariane_soc::IdWidthSlave ),
-    //     .AXI_USER_WIDTH ( AXI_USER_WIDTH           )
-    // ) dram_up ();
-
-    // AXI_BUS #(
-    //     .AXI_ADDR_WIDTH ( AXI_ADDRESS_WIDTH        ),
-    //     .AXI_DATA_WIDTH ( AXI_DATA_WIDTH           ),
-    //     .AXI_ID_WIDTH   ( ariane_soc::IdWidthSlave ),
-    //     .AXI_USER_WIDTH ( AXI_USER_WIDTH           )
-    // ) dram ();
+    ) bus();
 
 
     logic                         req;
@@ -178,88 +69,6 @@ module ariane_bare_tb #(
     logic [AXI_USER_WIDTH-1:0]    wuser;
     logic [AXI_USER_WIDTH-1:0]    ruser;
 
-    // axi_cut_intf #(
-    //     .BYPASS ( 1'b0 ),
-    //     .ADDR_WIDTH ( AXI_ADDRESS_WIDTH ),
-    //     .DATA_WIDTH ( AXI_DATA_WIDTH ),
-    //     .ID_WIDTH ( ariane_soc::IdWidthSlave ),
-    //     .USER_WIDTH ( AXI_USER_WIDTH )
-    // ) i_axi_cut_bus_side (
-    //     .clk_i ( clk_i ),
-    //     .rst_ni ( rst_ni ),
-    //     .in ( master[ariane_soc::DRAM] ),
-    //     .out ( dram_up )
-    // );
-
-    // axi_riscv_atomics_wrap #(
-    //     .AXI_ADDR_WIDTH ( AXI_ADDRESS_WIDTH        ),
-    //     .AXI_DATA_WIDTH ( AXI_DATA_WIDTH           ),
-    //     .AXI_ID_WIDTH   ( ariane_soc::IdWidthSlave ),
-    //     .AXI_USER_WIDTH ( AXI_USER_WIDTH           ),
-    //     .AXI_MAX_WRITE_TXNS ( 1  ),
-    //     .RISCV_WORD_WIDTH   ( 64 )
-    // ) i_axi_riscv_atomics (
-    //     .clk_i  ( clk_i                    ),
-    //     .rst_ni ( rst_n                    ),
-    //     .slv    ( master[ariane_soc::DRAM] ),
-    //     .mst    ( dram                     )
-    // );
-
-    // AXI_BUS #(
-    //     .AXI_ADDR_WIDTH ( AXI_ADDRESS_WIDTH        ),
-    //     .AXI_DATA_WIDTH ( AXI_DATA_WIDTH           ),
-    //     .AXI_ID_WIDTH   ( ariane_soc::IdWidthSlave ),
-    //     .AXI_USER_WIDTH ( AXI_USER_WIDTH           )
-    // ) dram_delayed();
-
-    // axi_delayer_intf #(
-    //     .AXI_ID_WIDTH        ( ariane_soc::IdWidthSlave ),
-    //     .AXI_ADDR_WIDTH      ( AXI_ADDRESS_WIDTH        ),
-    //     .AXI_DATA_WIDTH      ( AXI_DATA_WIDTH           ),
-    //     .AXI_USER_WIDTH      ( AXI_USER_WIDTH           ),
-    //     .STALL_RANDOM_INPUT  ( StallRandomInput         ),
-    //     .STALL_RANDOM_OUTPUT ( StallRandomOutput        ),
-    //     .FIXED_DELAY_INPUT   ( 0                        ),
-    //     .FIXED_DELAY_OUTPUT  ( 0                        )
-    // ) i_axi_delayer (
-    //     .clk_i  ( clk_i        ),
-    //     .rst_ni ( ndmreset_n   ),
-    //     .slv    ( dram         ),
-    //     .mst    ( dram_delayed )
-    // );
-    // axi_cut_intf #(
-    //     .BYPASS ( 1'b0 ),
-    //     .ADDR_WIDTH ( AXI_ADDRESS_WIDTH ),
-    //     .DATA_WIDTH ( AXI_DATA_WIDTH ),
-    //     .ID_WIDTH ( ariane_soc::IdWidthSlave ),
-    //     .USER_WIDTH ( AXI_USER_WIDTH )
-    // ) i_axi_cut_dram_side (
-    //     .clk_i ( clk_i ),
-    //     .rst_ni ( rst_ni ),
-    //     .in ( dram_atom ),
-    //     .out ( dram_mem )
-    // );
-    // logic t_r_vld, t_r_rdy, t_ar_vld, t_ar_rdy;
-    // logic [AXI_ADDRESS_WIDTH - 1 : 0] t_ar_addr;
-    // logic [AXI_DATA_WIDTH - 1 : 0] t_r_data;
-
-    // always_ff @(posedge clk_i) begin
-    //     if (verbose) begin
-    //         t_r_vld <= dram.r_valid;
-    //         t_r_rdy <= dram.r_ready;
-    //         t_ar_vld <= dram.ar_valid;
-    //         t_ar_rdy <= dram.ar_ready;
-    //         t_ar_addr <= dram.ar_addr;
-    //         t_r_data <= dram.r_data;
-    //         if(t_r_vld && t_r_rdy) begin
-    //             $display("@%d rdata: %h", cycle_count, t_r_data);
-    //         end
-    //         if (t_ar_vld && t_ar_rdy) begin
-    //             $display("@%d raddr: %h", cycle_count, t_ar_addr);
-    //         end
-    //     end
-    // end
-
     axi2mem #(
         .AXI_ID_WIDTH   ( ariane_soc::IdWidthSlave ),
         .AXI_ADDR_WIDTH ( AXI_ADDRESS_WIDTH        ),
@@ -268,7 +77,7 @@ module ariane_bare_tb #(
     ) i_axi2mem (
         .clk_i  ( clk_i        ),
         .rst_ni ( rst_n        ),
-        .slave  ( master[ariane_soc::DRAM]         ),
+        .slave  ( bus          ),
         .req_o  ( req          ),
         .we_o   ( we           ),
         .addr_o ( addr         ),
@@ -279,6 +88,29 @@ module ariane_bare_tb #(
         .data_i ( rdata        )
     );
 
+    function logic isInRange(logic [AXI_ADDRESS_WIDTH - 1: 0] in_addr,
+                                logic [AXI_ADDRESS_WIDTH - 1 : 0] base,
+                                logic [AXI_ADDRESS_WIDTH - 1 : 0] length);
+        return (in_addr >= base) && (in_addr < base + length);
+    endfunction
+    function logic[AXI_ADDRESS_WIDTH - 1 : 0] adjustedRange(
+                                logic [AXI_ADDRESS_WIDTH - 1: 0] in_addr,
+                                logic [AXI_ADDRESS_WIDTH - 1 : 0] base,
+                                logic [AXI_ADDRESS_WIDTH - 1 : 0] length);
+        return isInRange(in_addr, base, length) ? in_addr - base : '0;
+    endfunction
+    wire [AXI_DATA_WIDTH - 1 : 0] rom_rdata;
+
+    bootrom i_bootrom (
+        .clk_i      ( clk_i     ),
+        .req_i      ( isInRange(addr, ariane_soc::ROMBase, ariane_soc::ROMLength) && req),
+        .addr_i     ( adjustedRange(addr, ariane_soc::ROMBase, ariane_soc::ROMLength)   ),
+        .rdata_o    ( rom_rdata )
+    );
+
+    wire [AXI_ADDRESS_WIDTH - 1 : 0] sram_addr = adjustedRange(addr, ariane_soc::DRAMBase, ariane_soc::DRAMLength);
+    wire [AXI_DATA_WIDTH - 1 : 0] sram_rdata;
+
     preloadable_sram #(
         .DATA_WIDTH ( AXI_DATA_WIDTH ),
         .USER_WIDTH ( AXI_USER_WIDTH ),
@@ -287,65 +119,38 @@ module ariane_bare_tb #(
     ) i_main_sram (
         .clk_i      ( clk_i                                                                       ),
         .rst_ni     ( rst_n                                                                       ),
-        .req_i      ( req                                                                         ),
-        .we_i       ( we                                                                          ),
-        .addr_i     ( addr[$clog2(NUM_WORDS)-1+$clog2(AXI_DATA_WIDTH/8):$clog2(AXI_DATA_WIDTH/8)] ),
+        .req_i      ( isInRange(addr, ariane_soc::DRAMBase, ariane_soc::DRAMLength) && req        ),
+        .we_i       ( isInRange(addr, ariane_soc::DRAMBase, ariane_soc::DRAMLength) && we         ),
+        .addr_i     ( sram_addr[$clog2(NUM_WORDS)-1+$clog2(AXI_DATA_WIDTH/8):$clog2(AXI_DATA_WIDTH/8)] ),
         .wuser_i    ( wuser                                                                       ),
         .wdata_i    ( wdata                                                                       ),
         .be_i       ( be                                                                          ),
         .ruser_o    ( ruser                                                                       ),
-        .rdata_o    ( rdata                                                                       )
+        .rdata_o    ( sram_rdata                                                                  )
     );
+    logic [AXI_ADDRESS_WIDTH - 1 : 0] addr_q;
+    always_ff @(posedge clk_i) addr_q <= addr;
+    assign rdata = isInRange(addr_q, ariane_soc::DRAMBase, ariane_soc::DRAMLength) ? sram_rdata : rom_rdata;
 
-    // ---------------
-    // AXI Xbar
-    // ---------------
-
-    axi_pkg::xbar_rule_64_t [ariane_soc::NB_PERIPHERALS-1:0] addr_map;
-
-    assign addr_map = '{
-        '{ idx: ariane_soc::Debug,    start_addr: ariane_soc::DebugBase,    end_addr: ariane_soc::DebugBase + ariane_soc::DebugLength       },
-        '{ idx: ariane_soc::ROM,      start_addr: ariane_soc::ROMBase,      end_addr: ariane_soc::ROMBase + ariane_soc::ROMLength           },
-        '{ idx: ariane_soc::CLINT,    start_addr: ariane_soc::CLINTBase,    end_addr: ariane_soc::CLINTBase + ariane_soc::CLINTLength       },
-        '{ idx: ariane_soc::PLIC,     start_addr: ariane_soc::PLICBase,     end_addr: ariane_soc::PLICBase + ariane_soc::PLICLength         },
-        '{ idx: ariane_soc::UART,     start_addr: ariane_soc::UARTBase,     end_addr: ariane_soc::UARTBase + ariane_soc::UARTLength         },
-        '{ idx: ariane_soc::Timer,    start_addr: ariane_soc::TimerBase,    end_addr: ariane_soc::TimerBase + ariane_soc::TimerLength       },
-        '{ idx: ariane_soc::SPI,      start_addr: ariane_soc::SPIBase,      end_addr: ariane_soc::SPIBase + ariane_soc::SPILength           },
-        '{ idx: ariane_soc::Ethernet, start_addr: ariane_soc::EthernetBase, end_addr: ariane_soc::EthernetBase + ariane_soc::EthernetLength },
-        '{ idx: ariane_soc::GPIO,     start_addr: ariane_soc::GPIOBase,     end_addr: ariane_soc::GPIOBase + ariane_soc::GPIOLength         },
-        '{ idx: ariane_soc::DRAM,     start_addr: ariane_soc::DRAMBase,     end_addr: ariane_soc::DRAMBase + ariane_soc::DRAMLength         }
-    };
-
-    localparam axi_pkg::xbar_cfg_t AXI_XBAR_CFG = '{
-        NoSlvPorts: unsigned'(ariane_soc::NrSlaves),
-        NoMstPorts: unsigned'(ariane_soc::NB_PERIPHERALS),
-        MaxMstTrans: unsigned'(1), // Probably requires update
-        MaxSlvTrans: unsigned'(1), // Probably requires update
-        FallThrough: 1'b0,
-        LatencyMode: axi_pkg::CUT_ALL_PORTS,
-        AxiIdWidthSlvPorts: unsigned'(ariane_soc::IdWidth),
-        AxiIdUsedSlvPorts: unsigned'(ariane_soc::IdWidth),
-        UniqueIds: 1'b0,
-        AxiAddrWidth: unsigned'(AXI_ADDRESS_WIDTH),
-        AxiDataWidth: unsigned'(AXI_DATA_WIDTH),
-        NoAddrRules: unsigned'(ariane_soc::NB_PERIPHERALS)
-    };
-
-    axi_xbar_intf #(
-        .AXI_USER_WIDTH ( AXI_USER_WIDTH          ),
-        .Cfg            ( AXI_XBAR_CFG            ),
-        .rule_t         ( axi_pkg::xbar_rule_64_t )
-    ) i_axi_xbar (
-        .clk_i                 ( clk_i      ),
-        .rst_ni                ( rst_n      ),
-        .test_i                ( '0         ),
-        .slv_ports             ( slave      ),
-        .mst_ports             ( master     ),
-        .addr_map_i            ( addr_map   ),
-        .en_default_mst_port_i ( '0         ),
-        .default_mst_port_i    ( '0         )
-    );
-
+    wire gpio_req = req && isInRange(addr, ariane_soc::GPIOBase, ariane_soc::GPIOLength);
+    wire gpio_we = req && we;
+    wire gpio_be = be;
+    always_ff @(posedge clk_i) begin
+        if (gpio_req) begin
+            if (gpio_we && be == {(AXI_DATA_WIDTH / 8){1'b1}}) begin
+                if (wdata[0] == 0) begin
+                    $display("@%d Wrong toHost code %h", cycle_count, wdata);
+                    $stop;
+                end else if (wdata != 1) begin
+                    $display("@%d test failed! toHost = %h", cycle_count, wdata);
+                    $stop;
+                end else begin
+                    $display("@%d test passed!", cycle_count);
+                    $finish;
+                end
+            end
+        end
+    end
     // ---------------
     // Core
     // ---------------
@@ -369,8 +174,31 @@ module ariane_bare_tb #(
 
     assign i_ariane.i_cva6.vlt_trace_commit = COMMIT_TRACE;
 
-    `AXI_ASSIGN_FROM_REQ(slave[0], axi_ariane_req)
-    `AXI_ASSIGN_TO_RESP(axi_ariane_resp, slave[0])
+    // AXI_BUS #(
+    //     .AXI_ADDR_WIDTH ( AXI_ADDRESS_WIDTH        ),
+    //     .AXI_DATA_WIDTH ( AXI_DATA_WIDTH           ),
+    //     .AXI_ID_WIDTH   ( ariane_soc::IdWidthSlave ),
+    //     .AXI_USER_WIDTH ( AXI_USER_WIDTH           )
+    // ) ariane_axi_bus();
+
+
+    // axi_multicut_intf #(
+    //     .ADDR_WIDTH ( AXI_ADDRESS_WIDTH ),
+    //     .DATA_WIDTH ( AXI_DATA_WIDTH ),
+    //     .ID_WIDTH   ( ariane_soc::IdWidth ),
+    //     .USER_WIDTH ( AXI_USER_WIDTH ),
+    //     .NUM_CUTS   ( 1 )
+    // ) i_bus_cut (
+    //     .clk_i(clk_i),
+    //     .rst_ni(rst_ni),
+    //     .in(ariane_axi_bus),
+    //     .out(bus)
+    // );
+
+
+
+    `AXI_ASSIGN_FROM_REQ(bus, axi_ariane_req)
+    `AXI_ASSIGN_TO_RESP(axi_ariane_resp, bus)
 
 endmodule
 
